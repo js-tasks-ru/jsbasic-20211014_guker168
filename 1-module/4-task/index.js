86d373bfabc95b2da5988f4b6feb4cf2ed37d1cc
Oleg Vashchan <guker168@gmail.com>
2021-10-19 14:13:22 +0300

    1.4 Create checkSpam function

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
+  // let regExp = new RegExp('1xbet|xxx', 'gi');
+  // return regExp.test(str);
+  let lowerStr = str.toLowerCase();
+  return lowerStr.includes('1xbet') || lowerStr.includes('xxx');
 }
