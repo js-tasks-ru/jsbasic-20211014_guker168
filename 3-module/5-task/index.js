7bf91999d6708785ef8d2757ee0ef605171a9d5e
Oleg Vashchan <guker168@gmail.com>
2021-10-26 11:01:34 +0300

    3.5 Create getMinMax function

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,18 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(" ");
+  let numArr = arr
+    .filter((el) => {
+      if (isNaN(parseInt(el))) {
+        return false;
+      } else {
+        return true;
+      }
+    })
+    .map((el) => +el);
+  let min = Math.min(...numArr);
+  let max = Math.max(...numArr);
+  return {
+    min: min,
+    max: max,
+  };
 }
