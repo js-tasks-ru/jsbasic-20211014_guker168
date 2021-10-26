fcdbf845c8b6a0debb58ae2f72b17ce8c5cbb960
Oleg Vashchan <guker168@gmail.com>
2021-10-26 10:18:26 +0300

    3.4 Create showSalary function

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,18 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filteredUsers = users.filter((el) => {
+    if (el.age <= age) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return filteredUsers
+    .map((el, index, arr) => {
+      if (index === arr.length - 1) {
+        return `${el.name}, ${el.balance}`;
+      } else {
+        return `${el.name}, ${el.balance}\n`;
+      }
+    })
+    .join("");
 }
