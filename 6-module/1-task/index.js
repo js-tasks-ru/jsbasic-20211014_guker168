1070d17e991a357b37a209f573e8ba0db0c798e1
Oleg Vashchan <guker168@gmail.com>
2021-11-09 20:18:28 +0300

    6.1 Create class UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,43 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+    let buttons = this.elem.querySelectorAll("button");
+    [...buttons].map((button) =>
+      button.addEventListener("click", this.onCLick)
+    );
   }
+  render() {
+    this.elem = document.createElement("div");
+
+    this.strings = this.rows.map(
+      (el) => `<tr><td>${el.name}</td>
+    <td>${el.age}</td>
+    <td>${el.salary}</td>
+    <td>${el.city}</td>
+    <td><button>X</button></td>
+    </tr>`
+    );
+    this.elem.innerHTML = `
+    <table>
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+    <tbody>
+    ${this.strings}
+    </tbody>
+    </table>`;
+  }
+
+  onCLick = (event) => {
+    let line = event.currentTarget.closest("tr");
+    line.remove();
+  };
 }
